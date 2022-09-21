561c7d394df6a52d7905dc4fba9e33476e69fed6
trungthuc <nguyenthianh1232345@gmail.com>
2022-09-21 21:00:21 +0700

    add percent...

--- src/member.js
+++ src/member.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Member() {
+
+    const [percent, setPercent] = useState(0);
+
+    const handleClick = () => {
+        //console.log("111");
+        setPercent( (percent) => percent += 1 );
+        console.log(percent);
+
+        document.getElementsByClassName('percent')[0].style.width = percent + 'px';
+    }
+
     return(
         <div className="member">
-            <img alt="bp-member" className="member" src="https://m.media-amazon.com/images/M/MV5BMTJmNGJmYTgtYjAxNy00YmMzLTk2YTYtMGIzMmUwNDMyMTY1XkEyXkFqcGdeQXVyMTEyMjM2NDc2._V1_.jpg" />
+            <img onClick={handleClick} alt="bp-member" className="member" src="https://m.media-amazon.com/images/M/MV5BMTJmNGJmYTgtYjAxNy00YmMzLTk2YTYtMGIzMmUwNDMyMTY1XkEyXkFqcGdeQXVyMTEyMjM2NDc2._V1_.jpg" />
             <p>name</p>
-            <div>..</div>
+            <div className="percent">..</div>
         </div>
     );
 }
