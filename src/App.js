53f78ef95449417138f96ebf8335110b7d2118ad
trungthuc <nguyenthianh1232345@gmail.com>
2022-09-21 20:37:53 +0700

    ranking app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Member from './member';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Ranking ... members:</h2>
+      <Member />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/member.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Member() {
+    return(
+        <div className="member">
+            <img alt="bp-member" className="member" src="https://m.media-amazon.com/images/M/MV5BMTJmNGJmYTgtYjAxNy00YmMzLTk2YTYtMGIzMmUwNDMyMTY1XkEyXkFqcGdeQXVyMTEyMjM2NDc2._V1_.jpg" />
+            <p>name</p>
+            <div>..</div>
+        </div>
+    );
+}
+
+export default Member;
