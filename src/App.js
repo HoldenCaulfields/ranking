79c2be4e09451eb257f92b3da3f77ac0e1f25e7a
trungthuc <nguyenthianh1232345@gmail.com>
2022-09-22 11:58:29 +0700

    demo 1st time...

--- src/App.js
+++ src/App.js
@@ -3,10 +3,24 @@ import './App.css';
 import Member from './member';
 
 function App() {
+
+  const arr = [
+    'https://m.media-amazon.com/images/M/MV5BMTJmNGJmYTgtYjAxNy00YmMzLTk2YTYtMGIzMmUwNDMyMTY1XkEyXkFqcGdeQXVyMTEyMjM2NDc2._V1_FMjpg_UX1000_.jpg',
+    'https://i-giaitri.vnecdn.net/2022/02/21/drivemycartop-1645410255-16454-4256-8779-1645414472.jpg',
+    'https://www.elle.vn/wp-content/uploads/2022/09/04/493971/NOPE-poster-Loyola-Phoenix.jpg',
+    'https://m.media-amazon.com/images/M/MV5BZTllMjI0ZGYtM2FmZC00ZmY4LTlkNTYtZThlOWQ1OGQyZTA3XkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_.jpg'
+  ]
+
   return (
     <div className="App">
       <h2>Ranking ... members:</h2>
-      <Member />
+      
+      <ul>
+      {arr.map(
+        (item, index) => <li key={index}> <Member img={item} idimg={index} /> </li>
+      )}
+      </ul>
+      
     </div>
   );
 }
--- src/member.js
+++ src/member.js
@@ -1,22 +1,21 @@
 import React, { useState } from "react";
 
-function Member() {
+function Member(props) {
 
     const [percent, setPercent] = useState(0);
 
-    const handleClick = () => {
-        //console.log("111");
+    const handleClick = (e) => {
+        console.log(e.target.id);
         setPercent( (percent) => percent += 1 );
         console.log(percent);
 
-        document.getElementsByClassName('percent')[0].style.width = percent + 'px';
+        document.getElementsByClassName('percent')[e.target.id].style.width = percent + 50 + 'px';
     }
 
     return(
         <div className="member">
-            <img onClick={handleClick} alt="bp-member" className="member" src="https://m.media-amazon.com/images/M/MV5BMTJmNGJmYTgtYjAxNy00YmMzLTk2YTYtMGIzMmUwNDMyMTY1XkEyXkFqcGdeQXVyMTEyMjM2NDc2._V1_.jpg" />
-            <p>name</p>
-            <div className="percent">..</div>
+            <img onClick={handleClick} alt="bp-member" className="member" src={props.img} id={props.idimg} />
+            <div className="percent">.</div>
         </div>
     );
 }
